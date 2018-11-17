6dc632db3cbb6739722441ac259924b8a30c0793
Milind M <milindmmanoj@outlook.com>
2018-11-17 21:49:35 +0530

    GR Lychee says Hello Worldd
    Hello World printed via USB Serial Port
    LED 0 blinks

---
+++ HelloWorldwithLEDBlink.c
@@ -0,0 +1,19 @@
+#include "mbed.h"
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+
+// main() runs in its own thread in the OS
+int main() {
+    printf("Hello World!\n\r");
+    while (true) {
+        
+        led1 = !led1;
+        wait(0.1);
+        //printf("Hello World!\n\r");
+        
+        
+    }
+}
+
